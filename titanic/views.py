96141eb88fb3c01a12381a86db6f4ead8750e403
lior <halifalior@gmail.com>
2020-09-28 15:04:24 +0300

    adding result page

--- titanic/views.py
+++ titanic/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'index.html')
+
+def result(request):
+    user_input_age =request.GET["age"]
+    return render(request,'result.html',{'age':user_input_age})
